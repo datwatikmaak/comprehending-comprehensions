8cb3956baa9787c58d8d8be1dc90794c5e7edcdb
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-18 03:59:34 +0200

    Some basic exercises with lists

--- 02-basic-comprehension-exercises/main.py
+++ 02-basic-comprehension-exercises/main.py
@@ -8,9 +8,24 @@ joined_string = "*".join(list_string)
 
 # ask the user to enter multiple hex numbers (digits 0-9, and a-f), separated by whitespace, and sum those numbers
 # 1234 ab56 ffe4
-hex_numbers = input("Please enter multiple hex number: ")
-list_of_hex_numbers = hex_numbers.split()
+hex_numbers = "1234 ab56 ffe4"
+
+numbers = int(''.join(i for i in hex_numbers if i.isdigit()))
+
+total = sum(int(digit) for digit in str(numbers))
+
+print(total)
 
 
 # ask the user to enter words separated with whitespace. show the number of non-whitespace characters in their input.
 # this is a test = 11 characters
+words = "this is a test"
+
+chars = 0
+for char in words:
+    if char != " ":
+        chars += 1
+    else:
+        continue
+
+print(chars)
