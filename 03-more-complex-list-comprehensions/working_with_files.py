470af25ea06f7b219fdde47dad34c8bf1f9e224e
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-19 10:52:00 +0200

    Added a text files

--- 03-more-complex-list-comprehensions/working_with_files.py
+++ 03-more-complex-list-comprehensions/working_with_files.py
@@ -5,7 +5,7 @@
 
 
 file = [one_line.split(":")[0]
-        for one_line in open("../password-file.txt")
+        for one_line in open("../00-project-files/linux-etc-passwd.txt")
         if not one_line.startswith("g")]
 
 print(file)
