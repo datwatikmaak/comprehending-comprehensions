2a91324bdddd845ffa8bafc25c8d27bc812f784c
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-19 09:38:39 +0200

    List comprehensions with functions

---
+++ 03-more-complex-list-comprehensions/working_with_functions.py
@@ -0,0 +1,9 @@
+def reverse_word(word):
+    return word[::-1]
+
+
+# met split() maak je een list van de woorden die de gebruiker invoert.
+words = input("Enter some words: ").split()
+
+reverse = [reverse_word(one_word)
+           for one_word in words]
