40e9e19b64ea06d9fcd18303273978e8d6f1db19
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-19 10:35:30 +0200

    Opening and filtering information from a text file

--- 03-more-complex-list-comprehensions/working_with_files.py
+++ 03-more-complex-list-comprehensions/working_with_files.py
@@ -1,4 +1,11 @@
-file = [one_line
-        for one_line in open("../sample.txt")]
+# file = [one_line
+#         for one_line in open("../sample.txt")]
+#
+# print(file)
+
+
+file = [one_line.split(":")[0]
+        for one_line in open("../password-file.txt")
+        if not one_line.startswith("g")]
 
 print(file)
