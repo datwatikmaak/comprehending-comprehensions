5ae641d4902caa20b1cd6ba7db0258fb6adb6054
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-20 08:39:30 +0200

    Final code exercise 1

--- 04-more-complex-comprehension-exercises/exercise_01.py
+++ 04-more-complex-comprehension-exercises/exercise_01.py
@@ -7,3 +7,17 @@
 # 5! = 5 \times 4 \times 3 \times 2 \times 1 = 120
 #
 # Oplossing is dan: 6 + 24 + 120 = 150
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+nums = [3, 4, 5]
+
+total = sum([factorial(num)
+             for num in nums])
+
+print(total)
