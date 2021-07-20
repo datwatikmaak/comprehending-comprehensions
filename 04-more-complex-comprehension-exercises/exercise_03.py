0ade82f5f43216fa8a6306e43b82a8dedc7cb2f7
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-20 08:51:08 +0200

    Final code exercise 3

--- 04-more-complex-comprehension-exercises/exercise_03.py
+++ 04-more-complex-comprehension-exercises/exercise_03.py
@@ -1,3 +1,10 @@
 # TODO: Create a dictionary (or a variant thereof -- such as Counter) in which the keys are the IP addresses and the
 #  values are the number of times that each IP address appeared in the file.  Use the resulting dict/Counter to print
 #  each IP address and its times
+import collections
+
+
+file = [one_line.split(" - ")[0]
+        for one_line in open("../00-project-files/mini-access-log.txt")]
+
+print(collections.Counter(file))
