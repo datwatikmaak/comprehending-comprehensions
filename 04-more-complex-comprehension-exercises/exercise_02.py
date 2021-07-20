0631a0ce7a54081eccbac3f21c015b2623f7d4c9
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-20 08:45:42 +0200

    Final code exercise 2

--- 04-more-complex-comprehension-exercises/exercise_02.py
+++ 04-more-complex-comprehension-exercises/exercise_02.py
@@ -1,1 +1,5 @@
 # TODO: From mini-access-log.txt, create a list of the IP addresses in that file.
+file = [one_line.split(" - ")[0]
+        for one_line in open("../00-project-files/mini-access-log.txt")]
+
+print(file)
