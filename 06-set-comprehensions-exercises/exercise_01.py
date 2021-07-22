6e1761b6d6965da917efd8d6d7d6a820eba1e141
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-22 07:12:23 +0200

    My solution for exercise_01

--- 06-set-comprehensions-exercises/exercise_01.py
+++ 06-set-comprehensions-exercises/exercise_01.py
@@ -1,1 +1,7 @@
 # Ask the user to enter some integers, separated by spaces.  Sum the different numbers they enter.
+numbers = input("Enter some integers, separated by spaces: ")
+
+sum_of_numbers = sum({int(num)
+                      for num in numbers.split()})
+
+print(sum_of_numbers)
