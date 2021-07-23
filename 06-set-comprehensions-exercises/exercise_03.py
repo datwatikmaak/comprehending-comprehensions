d5f035a6bee3aac5af4d30fb2bd4e44f48f2cbc9
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-23 11:12:47 +0200

    Solution for exercise_03

--- 06-set-comprehensions-exercises/exercise_03.py
+++ 06-set-comprehensions-exercises/exercise_03.py
@@ -1,1 +1,8 @@
 # From linux-etc-passwd.txt, show all of the different shells.
+filename = "../00-project-files/linux-etc-passwd.txt"
+
+shells = {line.rstrip().split(":")[-1]
+          for line in open(filename)
+          if not line.startswith("#") and line.strip()}
+
+print(shells)
