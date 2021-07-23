a54c7e402db8eb7207a70143cd7c73807f61aa83
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-23 11:05:50 +0200

    Solution for exercise_02

--- 06-set-comprehensions-exercises/exercise_02.py
+++ 06-set-comprehensions-exercises/exercise_02.py
@@ -1,1 +1,8 @@
 # From the file nums.txt, sum all of the distinct numbers there.
+filename = "../00-project-files/nums.txt"
+
+sum_of_numbers = sum({int(one_line)
+                      for one_line in open(filename)
+                      if one_line.strip()})
+
+print(sum_of_numbers)
