4698cabd1f7d19ca044a7cb1b2c6f6eb729fa117
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-25 11:05:12 +0200

    Dict comprehensions exercises #4 final code

--- 08-dict-comprehension-exercises/exercise_04.py
+++ 08-dict-comprehension-exercises/exercise_04.py
@@ -1,4 +1,17 @@
-# Create a set comprehension from words.txt (the Linux dictionary).
+# Create a set comprehension from words (the Linux dictionary).
 #
-# Use that set comprehension to create a dict (using a dict comprehension).  Ask the user to enter a sentence.  The
-# output dict will be the user's words, and the values will be True/False, indicating if the word is in the dictionary.
+# Use that set comprehension to create a dict (using a dict comprehension). Ask the user to enter a sentence. The
+# output dict will be the user's words, and the values will be True/False, indicating if the word is in the
+# dictionary.
+
+filename = "../00-project-files/words"
+
+set_of_words = {one_word.strip()
+                for one_word in open(filename)}
+
+words = input("Enter a sentence: ")
+
+word_in_sentence = {one_word: one_word in set_of_words
+                    for one_word in words.lower().split()}
+
+print(word_in_sentence)
