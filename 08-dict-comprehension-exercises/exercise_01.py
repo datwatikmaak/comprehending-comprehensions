5a01ff3e51dcee8c0d69b536d4ffe0dfb967dccc
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-25 10:37:10 +0200

    Dict comprehensions exercises #1 final code

--- 08-dict-comprehension-exercises/exercise_01.py
+++ 08-dict-comprehension-exercises/exercise_01.py
@@ -1,2 +1,14 @@
 # Ask the user for a sentence, and then create a dict based on that sentence, in which the keys are the words and the
 # values are the "scores" for the words.
+
+def word_score(word):
+    return sum([ord(letter) - 96
+                for letter in word.lower()])
+
+
+words = input("Please enter some words: ")
+
+score_dict = {one_word: word_score(one_word)
+              for one_word in words.split()}
+
+print(score_dict)
