223d4b4b51c711e1f79fb70aaa7c85323565838a
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-25 10:49:44 +0200

    Dict comprehensions exercises #3 final code

--- 08-dict-comprehension-exercises/exercise_03.py
+++ 08-dict-comprehension-exercises/exercise_03.py
@@ -1,1 +1,8 @@
 # Flip a dict, so the keys become values and vice versa.
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
+flipped_dict = {value : key
+                for key, value in d.items()}
+
+print(flipped_dict)
