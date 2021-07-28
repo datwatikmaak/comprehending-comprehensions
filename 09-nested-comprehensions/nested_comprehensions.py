f4b5a1e3e1aa59f98facec079744ed372289a9f1
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-28 10:18:24 +0200

    Nested comprehensions learning

---
+++ 09-nested-comprehensions/nested_comprehensions.py
@@ -0,0 +1,16 @@
+my_list = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
+
+joined_list = [one_item
+               for sublist in my_list
+               for one_item in sublist]
+
+# joined_list = [one_item
+#                for one_item in my_list]
+
+print(joined_list)
+
+print([(x, y)
+       for x in range(5)
+       if not x % 2
+       for y in range(5)
+       if y % 2])
