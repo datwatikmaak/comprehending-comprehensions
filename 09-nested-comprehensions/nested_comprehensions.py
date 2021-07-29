1aa0923a197c4d2d744513b814997e1d9e1a6f74
Leonieke <54051901+datwatikmaak@users.noreply.github.com>
2021-07-29 14:08:53 +0200

    Added the print output in comments

--- 09-nested-comprehensions/nested_comprehensions.py
+++ 09-nested-comprehensions/nested_comprehensions.py
@@ -7,10 +7,12 @@ joined_list = [one_item
 # joined_list = [one_item
 #                for one_item in my_list]
 
-print(joined_list)
+print(joined_list)    # [10, 20, 30, 40, 50, 60, 70, 80, 90]
 
 print([(x, y)
        for x in range(5)
        if not x % 2
        for y in range(5)
        if y % 2])
+
+# [(0, 1), (0, 3), (2, 1), (2, 3), (4, 1), (4, 3)]
